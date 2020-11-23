62d96cfe53546d70012f0939553cfc8296753d5f
Duncan McGreggor <oubiwann@gmail.com>
2020-11-22 23:46:47 -0600

    Added supervisor boilerplate.

--- src/osc_client_mgr_sup.erl
+++ src/osc_client_mgr_sup.erl
@@ -1,1 +1,29 @@
 -module(osc_client_mgr_sup).
+
+-behaviour(supervisor).
+
+-define(MGR, osc_client_mgr). 
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% @doc Initializes the supervisor.
+%% @spec init(Args) -> {ok, {SupFlags, ChildSpecs}} | ignore | {error, Reason}
+init([]) ->
+    Server = #{
+        id => ?MGR,
+        start => {?MGR, start_link, []},
+	restart => permanent,
+        shutdown => 2000,
+        type => worker,
+        modules => [?MGR]},
+    {ok, {#{
+          strategy => one_for_one,
+          intensity => 3,
+          period => 60}, [Server]}}.
